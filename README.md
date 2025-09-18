65296e158031c5ce647c75f4be2698a8163d75df
DANIEL TALPAS <73405858+kvothe321@users.noreply.github.com>
2025-09-18 21:02:27 +0300

    init: create gradle plugin project

---
+++ README.md
@@ -0,0 +1,1 @@
+![Project Icon](.idea/icon.svg)
---
+++ plugin/build.gradle.kts
@@ -0,0 +1,70 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `java-gradle-plugin`
+    alias(libs.plugins.kotlin.jvm)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+@Suppress("UnstableApiUsage")
+testing {
+    suites {
+        // Configure the built-in test suite
+        val test by getting(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest("2.2.0")
+        }
+
+        // Create a new test suite
+        val functionalTest by registering(JvmTestSuite::class) {
+            // Use Kotlin Test test framework
+            useKotlinTest("2.2.0")
+
+            dependencies {
+                // functionalTest test suite depends on the production code in tests
+                implementation(project())
+            }
+
+            targets {
+                all {
+                    // This test suite should run after the built-in test suite has run its tests
+                    testTask.configure { shouldRunAfter(test) }
+                }
+            }
+        }
+    }
+}
+
+gradlePlugin {
+    // Define the plugin
+    val greeting by plugins.creating {
+        id = "com.tlpcraft.plugin.commitlint.greeting"
+        implementationClass = "com.tlpcraft.plugin.commitlint.CommitlintPlugin"
+    }
+}
+
+gradlePlugin.testSourceSets.add(sourceSets["functionalTest"])
+
+tasks.named<Task>("check") {
+    // Include functionalTest as part of the check lifecycle
+    dependsOn(testing.suites.named("functionalTest"))
+}
